1fa1c795cc2b5c5c8a9659a5b13b026600793c5d
epurinithin10 <epurinithin1100@gmail.com>
2026-03-25 18:04:58 +0530

    UC-5: Implemented enhanced for-loop for multiple arguments

--- HelloApp.java
+++ HelloApp.java
@@ -2,14 +2,26 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        // Default name
-        String name = "World";
-
-        // Check if command-line argument is provided
-        if (args.length > 0) {
-            name = args[0];
+        // Case 1: No arguments
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
         }
 
-        System.out.println("Hello, " + name + "!");
+        // Case 2: Multiple names using enhanced for loop
+        StringBuilder nameBuilder = new StringBuilder();
+        boolean first = true;
+
+        for (String name : args) {
+
+            if (!first) {
+                nameBuilder.append(", ");
+            }
+
+            nameBuilder.append(name);
+            first = false;
+        }
+
+        System.out.println("Hello, " + nameBuilder + "!");
     }
 }
