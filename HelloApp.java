894ed70c0d8cd3f38b7a1f65e54f184727a8bb9a
epurinithin10 <epurinithin1100@gmail.com>
2026-03-25 17:50:32 +0530

    UC-4: Added loop to handle multiple command-line arguments

--- HelloApp.java
+++ HelloApp.java
@@ -2,14 +2,27 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        // Default name
-        String name = "World";
+        String greeting;
 
-        // Check if command-line argument is provided
-        if (args.length > 0) {
-            name = args[0];
+        // If no names provided
+        if (args.length == 0) {
+            greeting = "World";
+        } else {
+
+            StringBuilder names = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+
+                names.append(args[i]);
+
+                if (i < args.length - 1) {
+                    names.append(", ");
+                }
+            }
+
+            greeting = names.toString();
         }
 
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + greeting + "!");
     }
 }
